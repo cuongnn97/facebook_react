fa0f07c3fdd8e6100ce7cb97946f98247ff94028
Cuong Nguyen <nncuong1101@gmail.com>
2021-04-06 17:49:14 +0700

    update

--- facebook-ui/src/Home.js
+++ facebook-ui/src/Home.js
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 }
